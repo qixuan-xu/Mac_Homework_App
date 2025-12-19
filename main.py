89c5e020ec36eaa79a0928988dd98aab747836b9
Qixuan Xu <qixuanxu@hotmail.com>
2025-12-18 20:35:55 -0500

    add:中间框，蓝色，文本输出改到中间

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QFrame
 from PySide6.QtWidgets import (
     QApplication,
     QWidget,
@@ -64,7 +65,39 @@ def main():
     middle.addWidget(prompt)
     middle.addWidget(textbox)
 
-    # ===== 3) 底部：按钮栏（水平布局，按钮靠右）=====
+    # ===== 3) 中间：文本  =====
+    middle_text = QLabel("这里是中间说明文字，可以写提示、说明或状态信息。")
+    middle_text.setWordWrap(True)
+    middle_text.setContentsMargins(16, 0, 16, 0)  # 文字和边框的内边距
+    middle_text.setFixedHeight(100)  # 胶囊高度（你可改 48/56/64）
+
+    middle_box = QFrame()
+    middle_box.setAttribute(Qt.WA_StyledBackground, True)  # ⭐ 让 QFrame 按样式画背景
+    middle_box.setFrameShape(QFrame.NoFrame)  # ⭐ 避免系统再画框
+    middle_box.setFrameShadow(QFrame.Plain)
+
+
+    middle_box.setStyleSheet("""
+        QFrame {
+            border: 2px solid #93C5FD;
+            border-radius: 16px;
+            background: transparent;
+        }
+    """)
+
+    middle_text.setStyleSheet("""
+        QLabel {
+            background-color: rgba(239, 246, 255, 0.12);
+            
+            border-radius: 12px;
+        }
+    """)#color: #000000;
+
+    box_layout = QVBoxLayout(middle_box)
+    box_layout.setContentsMargins(0,0,0,0)
+    box_layout.addWidget(middle_text)
+
+    # ===== 4) 底部：按钮栏（水平布局，按钮靠右）=====
     label = QLabel("Hello from my Mac app!")
     label.setWordWrap(True)  # 文本长了自动换行（更像 App）
 
@@ -73,9 +106,9 @@ def main():
     def on_button_clicked():
         entered_text = textbox.text().strip()
         if not entered_text:
-            label.setText("你还没输入内容。")
+            middle_text.setText("你还没输入内容。")
         else:
-            label.setText(f"已提交：{entered_text}")
+            middle_text.setText(f"已提交：{entered_text}")
 
     button.clicked.connect(on_button_clicked)
 
@@ -89,9 +122,13 @@ def main():
     root.setContentsMargins(30, 20, 30, 20)  # 内边距（更像 App）
     root.setSpacing(12)                      # 控件间距
 
+
     root.addLayout(top_bar)
     root.addLayout(middle)
-    root.addStretch()        # 中间区域和底部区域之间留弹性空间
+    root.addSpacing(10)
+    #root.addWidget(middle_text)  # 中间的“文字说明”
+    root.addWidget(middle_box)
+    root.addStretch()
     root.addLayout(bottom_bar)
 
     window.setLayout(root)
